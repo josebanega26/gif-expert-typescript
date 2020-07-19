d6f3405360d3a233c70750b7abe56248ee18a0e9
Jose Banega <banegajose26@gmail.com>
2020-07-19 00:26:46 -0300

    Add footer component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    const year = new Date().getFullYear()
+    return (
+        <footer>
+            Made by Jose Banega {year} - Fernando Herrera React Course
+        </footer>
+    )
+}
+
+export default Footer
