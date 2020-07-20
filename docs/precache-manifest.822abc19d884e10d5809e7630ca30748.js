5b1996f2f9ae6eed3e5655666b9622c64caf495c
Jose Banega <banegajose26@gmail.com>
2020-07-19 23:59:51 -0300

    Add docs for github pages & update Readme.md

---
+++ docs/precache-manifest.822abc19d884e10d5809e7630ca30748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c14be049a27a87d9cfec60524bb1e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48855365dc3b7a4b551",
+    "url": "/static/css/main.f1bece36.chunk.css"
+  },
+  {
+    "revision": "cec30beedda70fa405d7",
+    "url": "/static/js/2.e881aa95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e881aa95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48855365dc3b7a4b551",
+    "url": "/static/js/main.4760fa1b.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
